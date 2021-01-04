3540ea64bdcccf01f9c4f7634177d3202c08e963
Modori <viper@etri.re.kr>
2021-01-04 18:07:06 +0900

    Add boilerplate in mw-nfd/*.hpp files #4

--- daemon/mw-nfd/input-thread.hpp
+++ daemon/mw-nfd/input-thread.hpp
@@ -1,4 +1,23 @@
 
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2019-2021,  HII of ETRI,
+ *
+ * This file is part of MW-NFD (Named Data Networking Multi-Worker Forwarding Daemon).
+ * See README.md for complete list of NFD authors and contributors.
+ *
+ * MW-NFD is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * MW-NFD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * NFD, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef NFD_IW_NFD_HPP
 #define NFD_IW_NFD_HPP
 
